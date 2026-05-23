ead8555d954ab82211d67361259d5cfe758e81b6
clnan1026 <wncksaa@naver.com>
2026-05-23 23:26:53 +0900

    Update API prototype with random squares #1

--- api/AI_api.rb
+++ api/AI_api.rb
@@ -3,16 +3,29 @@
 require "json"
 
 def generate_sugoroku_map(theme)
-  {
-    title: "#{theme} スゴロク",
-    squares: [
-      "スタート",
-      "#{theme}について話す",
-      "2マス進む",
-      "1回休み",
-      "ゴール"
+
+    events = [
+        "2マス進む",
+        "１回休み",
+        "3マス戻る",
+        "サイコロをもう一回振る",
+        "宝箱を見つける"
     ]
-  }
+
+    random_squares = 3.times.map {events.sample}
+
+    result =  {
+        :title => "#{theme} スゴロク",
+        :squares =>  [
+        "スタート",
+        "#{theme}について話す",
+        *random_squares,
+        "ゴール"
+    ]
+}
+
+result
+
 end
 
 result = generate_sugoroku_map("旅行")
