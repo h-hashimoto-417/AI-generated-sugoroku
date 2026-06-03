81ea5c1d0255bcec655c77608fe850ebbe7823c7
h-hashimoto-417 <hitomi.seyomo484@outlook.jp>
2026-06-03 17:24:08 +0900

    mapデータを読み込む

--- framework/app/controllers/play_controller.rb
+++ framework/app/controllers/play_controller.rb
@@ -3,9 +3,30 @@ class PlayController < ApplicationController
     if session[:map_id]
       @map = Map.find_by(id: session[:map_id])
       @squares = @map&.squares&.order(:position)
+      @sugoroku = {
+        "title" => @map.map_name,
+        "squares" => @map.squares.order(:position).map do |square|
+            {
+            "type" => square.square_type,
+            "text" => square.square_text,
+            "effect" => square.effect,
+            "value" => square.value
+            }
+       end
+}
     end
   end
 
   def input_member
+    player_count = params[:player_count].to_i
+    for i in 1..player_count
+        Player.create!(
+            game_id: 1, # 仮のゲームID、実際には適切なゲームIDを設定する必要があります
+            user_id: i # 仮のユーザーID、実際には適切なユーザーIDを設定する必要があります
+            player_name: "Player #{i}" # 仮のプレイヤー名、実際にはユーザーから入力を受け取るなどして設定する必要があります
+        )
+        end
   end
+
+
 end
